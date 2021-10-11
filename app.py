6dee8a19e1e73abeca569fb7e13436f5bd2ca280
rajivsiddiqui <38841463+rajivsiddiqui@users.noreply.github.com>
2021-10-11 10:40:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Rajiv Siddiqui's  Udacity Final project Blue Deployment Version 1"
-app.run(host="0.0.0.0", port=8080, debug=True)
+app.run(host="0.0.0.0", port=8080, debug=Truess)
